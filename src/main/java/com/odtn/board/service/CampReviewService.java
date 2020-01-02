a39df5e0ffb1f336db1a2c6c0dbf0e04ac2a7b1c
KJS <gmx0807@gmail.com>
2020-01-02 16:15:45 +0900

    2020-01-02-merge-board-admin

--- src/main/java/com/odtn/board/service/CampReviewService.java
+++ src/main/java/com/odtn/board/service/CampReviewService.java
@@ -6,23 +6,31 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.odtn.member.dto.MemberDto;
 
-import jdk.internal.module.IllegalAccessLogger.Mode;
-
 public interface CampReviewService {
-	//리뷰작성
-	public void write(ModelAndView mav,HttpSession session,MemberDto memberDto);
-	//리뷰작성확인
-	public void writeOk(ModelAndView mav,MemberDto memberDto);
-	//목록
-	public void list(ModelAndView mav,HttpSession session,MemberDto memberDto);
-	//검색목록
+	// 리뷰작성
+	public void write(ModelAndView mav, HttpSession session,
+			MemberDto memberDto);
+
+	// 리뷰작성확인
+	public void writeOk(ModelAndView mav, MemberDto memberDto);
+
+	// 목록
+	public void list(ModelAndView mav, HttpSession session,
+			MemberDto memberDto);
+
+	// 검색목록
 	public void searchList(ModelAndView mav);
-	//읽기
+
+	// 읽기
 	public void read(ModelAndView mav);
-	//삭제
+
+	// 삭제
 	public void delete(ModelAndView mav);
-	//수정
-	public void update(ModelAndView mav,MemberDto memberDto,HttpSession session);
-	//수정확인
+
+	// 수정
+	public void update(ModelAndView mav, MemberDto memberDto,
+			HttpSession session);
+
+	// 수정확인
 	public void updateOk(ModelAndView mav);
 }
